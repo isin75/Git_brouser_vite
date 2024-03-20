fb4053056dcfc47adea69b57daa7f9d1745084ad
isin75 <jon.s124524@gmail.com>
2024-03-20 18:35:48 +0200

    fix vercel url

--- client/main.jsx
+++ client/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import './index.css'
 
 import store from './store/store'
-import router from './config/routs'
 import Router from './config/routs'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
