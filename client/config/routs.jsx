778abcdfb372cd066b130e565679df233097c4ec
isin75 <jon.s124524@gmail.com>
2024-03-20 18:58:25 +0200

    fix vercel url

--- client/config/routs.jsx
+++ client/config/routs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Layout from '../Layout'
 import User, { repoLoader } from '../src/components/user/user'
@@ -7,16 +7,18 @@ import Repo, { readLoader } from '../src/components/repo/repo'
 import SearchBlock from '../src/components/searchBlock/searchBlock'
 import Home from '../src/pages/home/home'
 
-const Router = () => {
+const Routers = () => {
   return (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<SearchBlock />} />
-      <Route path="/:userName" element={<User />} loader={repoLoader} />
-      <Route path="/:userName/:repository" element={<Repo />} loader={readLoader} />
-      <Route path="home" element={<Home />} />
-    </Route>
-  </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<SearchBlock />} />
+          <Route path=":userName" element={<User />} loader={repoLoader} />
+          <Route path=":userName/:repository" element={<Repo />} loader={readLoader} />
+          <Route path="home" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
-export default Router
+export default Routers
